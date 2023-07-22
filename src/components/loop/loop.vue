e36c8cbc86626794d17596e1fc2f1e864481332c
Ashik <ashikulislam384@gmail.com>
2023-07-22 18:52:17 +0600

    array loop width data

--- src/components/loop/loop.vue
+++ src/components/loop/loop.vue
@@ -1,14 +1,21 @@
 <script setup>
-const items = [1, 2, 3, 4, 5, 6];
+// const items = [1, 2, 3, 4, 5, 6];
 //List Rendaring - means in vue when we render loop 
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+  "Hello fulir ma",
+];
 </script>
 
 <template>
-  <section
-    class="mx-auto container p-10 flex items-center text-center flex-col space-y-2" >
-    <div class="p-5 border border-gray-600 w-40" v-for="(item, index) in items" :key="index" >
-      {{ item }}
-    </div>
+  <section class="mx-auto container p-40">
+    <section class="mx-auto container space-y-3">
+        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="index"> {{ index+1 }}. {{ task }} </div>
+    </section>
   </section>
 </template>
 
