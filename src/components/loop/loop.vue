58bc86e5490e28790a046cb3732c8b60e9c7dab8
Ashik <ashikulislam384@gmail.com>
2023-07-22 20:49:30 +0600

    Filter In loop

--- src/components/loop/loop.vue
+++ src/components/loop/loop.vue
@@ -1,20 +1,37 @@
 <script setup>
 // const items = [1, 2, 3, 4, 5, 6];
 //List Rendaring - means in vue when we render loop 
+// const tasks = [
+//   "Complete project proposal",
+//   "Update website content",
+//   "Fix bug in user authentication",
+//   "Prepare presentation slides",
+//   "Test new feature implementation",
+//   "Hello fulir ma",
+// ];
+
 const tasks = [
-  "Complete project proposal",
-  "Update website content",
-  "Fix bug in user authentication",
-  "Prepare presentation slides",
-  "Test new feature implementation",
-  "Hello fulir ma",
+    { id: 1, title: "Complete project proposal - p", status: "In Progress" },
+    { id: 2, title: "Update website content - c", status: "Completed" },
+    { id: 3, title: "Fix bug in user authentication -p", status: "In Progress" },
+    { id: 4, title: "Prepare presentation slides -pen", status: "Pending" },
+    { id: 5, title: "Test new feature implementation -p", status: "In Progress" },
 ];
+
+
+function getCompletedTasks() {
+    return tasks.filter( function(task){
+        return  "In Progress" == task.status
+    })
+}
+
 </script>
 
 <template>
   <section class="mx-auto container p-40">
     <section class="mx-auto container space-y-3">
-        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="index"> {{ index+1 }}. {{ task }} </div>
+        <!-- <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="task.id">{{ task.id }}. {{ task.title }} </div> -->
+        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in getCompletedTasks()" :key="task.id">{{ task.id }}. {{ task.title }} </div>
     </section>
   </section>
 </template>
