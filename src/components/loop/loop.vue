8272ef92e4b2ccf9121d6adefbc5116fda0ffe5f
Ashik <ashikulislam384@gmail.com>
2023-07-22 20:54:55 +0600

    Conditonal Class Binding

--- src/components/loop/loop.vue
+++ src/components/loop/loop.vue
@@ -16,14 +16,16 @@ const tasks = [
     { id: 3, title: "Fix bug in user authentication -p", status: "In Progress" },
     { id: 4, title: "Prepare presentation slides -pen", status: "Pending" },
     { id: 5, title: "Test new feature implementation -p", status: "In Progress" },
+    { id: 6, title: "Update website content - c", status: "Completed" },
+    { id: 7, title: "Test new feature implementation -p", status: "In Progress" },
 ];
 
 
-function getCompletedTasks() {
-    return tasks.filter( function(task){
-        return  "In Progress" == task.status
-    })
-}
+// function getCompletedTasks() {
+//     return tasks.filter( function(task){
+//         return  "Completed" == task.status
+//     })
+// }
 
 </script>
 
@@ -31,7 +33,7 @@ function getCompletedTasks() {
   <section class="mx-auto container p-40">
     <section class="mx-auto container space-y-3">
         <!-- <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="task.id">{{ task.id }}. {{ task.title }} </div> -->
-        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in getCompletedTasks()" :key="task.id">{{ task.id }}. {{ task.title }} </div>
+        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="task.id" :class=" task.status == 'Completed'? 'bg-red-400' : '' ">{{ task.id }}. {{ task.title }} </div>
     </section>
   </section>
 </template>
