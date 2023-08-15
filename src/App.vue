b2b4f89b2bcbe0b0c4d5376ecc969a88bc074512
Ashik <ashikulislam384@gmail.com>
2023-08-15 11:41:50 +0600

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import navBar from './components/navBar.vue';
-import headerSection from './components/headerSection.vue'
-import AboutSection from './components/aboutSection.vue';
-import ManuSection from './components/manuSection.vue';
-import classBind from './components/classBinding/classBind.vue';
-import styleBinding from './components/styleBinding/styleBinding.vue';
-import Loop from './components/loop/loop.vue';
-import event from './components/event/event.vue';
-import Accordion from './components/accordion/accordion.vue';
-import Chunkmorris from './components/chunkmorris/chunkmorris.vue';
+import Model from './components/model/model.vue';
 </script>
 
 <template>
 
- <Chunkmorris/>
+ <Model />
 
 </template>
 
---
+++ src/components/model/model.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { ref } from 'vue'
+    let color = ref('')
+
+    function getstyle(){
+        return{
+            backgroundColor: color.value
+        }
+    }
+</script>
+
+<template>
+    <section class="mx-auto container">
+        <h1 class="text-2xl mb-10">Vue Form</h1>
+        <p class="pb-5">Your Favorite Color: {{ color }}</p>
+        <p> Your Favorite Color: 
+        <input class="p-5 border-2" v-model="color" />
+        <div class="w-32 h-32 mt-10" :style="getstyle()"></div>
+        </p>
+    </section>
+</template>
+
+<style scoped>
+    
+</style>
